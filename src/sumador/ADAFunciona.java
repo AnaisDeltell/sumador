3b8e766b2c3d1f7f46817064ebb0cb756f2cd556
Anais <anaisdeltellanton@gmail.com>
2022-05-22 00:35:56 +0200

    Finalizado

---
+++ src/sumador/ADAFunciona.java
@@ -0,0 +1,12 @@
+package sumador;
+
+public class ADAFunciona {
+
+	public static void main(String[] args) {
+		//String valor = args[1];	
+		String valor = "05";
+		 ASumar sumi = new ASumar();
+	     String resultado = sumi.sumativo(valor);
+	     System.out.println(resultado);
+	}
+};
